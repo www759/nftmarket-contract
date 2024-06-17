af1a7a83615d84a7e752231a5681b1d9ff708fd0
www759 <2629789522@qq.com>
2024-06-17 19:35:00 +0800

    add read and corresponding test

--- test/market-test.js
+++ test/market-test.js
@@ -22,30 +22,33 @@ describe('NftMarket', async function() {
         await nft.setApprovalForAll(market.target, true);
     })
 
-    it("market owner check", async () => {
-        expect(await market.marketOwner()).to.equal(accountA);
-    })
+    it("erc20 check", async () => {
+        expect(await erc20.decimals()).to.equal(18);
 
-    it("erc20 address check", async () => {
-        expect(await market.erc20()).to.equal(erc20.target);
-    })
-
-    it("account A and B should have erc20", async() => {
         expect(await erc20.balanceOf(accountA.address)).to.equal(erc20Num);
         expect(await erc20.balanceOf(accountB.address)).to.equal(erc20Num);
-    })
 
-    it("accountB should approve erc20 to makret", async() => {
         expect(await erc20.allowance(accountB.address, market)).to.equal(erc20Num);
     })
 
-    it("accountA should approve nft to makret", async() => {
+    it("nft check", async () => {
         expect(await nft.isApprovedForAll(accountA.address, market)).to.equal(true);
+
+        expect(await nft.balanceOf(accountA.address)).to.equal(3);
+        
+        expect(await nft.tokenURI(0)).to.equal("0");
+        expect(await nft.tokenURI(1)).to.equal("1");
+        expect(await nft.tokenURI(2)).to.equal("2");
+
+    })
+    
+
+    it("market owner and erc20 address check", async () => {
+        expect(await market.marketOwner()).to.equal(accountA);
+
+        expect(await market.erc20()).to.equal(erc20.target);
     })
 
-    it("accountA should have 3 nft", async() => {
-        expect(await nft.balanceOf(accountA.address)).to.equal(3);
-    })
 
     it("market func check", async() => {
         const price = "1000000000000000000000";
@@ -55,22 +58,32 @@ describe('NftMarket', async function() {
         expect(await market.listNFT(nft.target, 0, price)).to.emit(market, "NewOrder");
         expect(await market.listNFT(nft.target, 1, price)).to.emit(market, "NewOrder");
         expect(await market.listNFT(nft.target, 2, price)).to.emit(market, "NewOrder");
+
+        
+        // isListed
+        expect(await market.isListed(nft.target, 0)).to.equal(true);
+        expect(await market.isListed(nft.target, 1)).to.equal(true);
+        expect(await market.isListed(nft.target, 2)).to.equal(true);
+
+        // getAll
+        expect((await market.getAllOrders(nft.target)).length).to.equal(3);
         
         // unlist
         expect(await market.getOrderLength(nft.target)).to.equal(3);
-        expect(await market.unlistNFT(nft.target, 2)).to.emit(market, "OrderCanceled");
+        expect(await market.unlistNFT(nft.target, 1)).to.emit(market, "OrderCanceled");
         expect(await market.getOrderLength(nft.target)).to.equal(2);
+        expect(await market.tokenIdToIndex(nft.target, 2)).to.equal(1);
+        
 
         // changePrice
-        expect(await market.changePrice(nft.target, 1, newPrice)).to.emit(market, "PriceChanged");
-        expect((await market.orderOfTokenId(nft.target, 1))[3]).to.equal(newPrice);
+        expect(await market.changePrice(nft.target, 2, newPrice)).to.emit(market, "PriceChanged");
+        expect((await market.orderOfTokenId(nft.target, 2))[3]).to.equal(newPrice);
 
         // buy
-        expect((await market.connect(accountB).buy(nft.target, 1))).to.emit(market, "Deal");
+        expect((await market.connect(accountB).buy(nft.target, 0))).to.emit(market, "Deal");
         expect(await market.getOrderLength(nft.target)).to.equal(1);
-        expect(await nft.ownerOf(1)).equal(accountB.address);
+        expect(await nft.ownerOf(0)).equal(accountB.address);
     })
 
-    
 
 })
